c40b3fd63a99962a8f0fdf5e3f2c8c8b4fc27339
Volt-Scripter <alsantiano2011@gmail.com>
2025-09-20 18:26:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const sections = document.querySelectorAll('section');
-  const cards = document.querySelectorAll('.doc-card');
-  const tables = document.querySelectorAll('table');
+  const sections=document.querySelectorAll('section');
+  const cards=document.querySelectorAll('.vertical-card');
+  const tables=document.querySelectorAll('table');
 
-  function scrollAnim() {
-    const triggerBottom = window.innerHeight / 5 * 4;
-    sections.forEach(section => {
-      if (section.getBoundingClientRect().top < triggerBottom) {
-        section.classList.add('show');
-      }
-    });
-    cards.forEach(card => {
-      if (card.getBoundingClientRect().top < triggerBottom) {
-        card.classList.add('show');
-      }
-    });
-    tables.forEach(table => {
-      if (table.getBoundingClientRect().top < triggerBottom) {
-        table.classList.add('show');
-      }
-    });
+  function scrollAnim(){
+    const trigger=window.innerHeight/5*4;
+    sections.forEach(s => {if(s.getBoundingClientRect().top<trigger) s.classList.add('show');});
+    cards.forEach(c => {if(c.getBoundingClientRect().top<trigger) c.classList.add('show');});
+    tables.forEach(t => {if(t.getBoundingClientRect().top<trigger) t.classList.add('show');});
   }
 
-  window.addEventListener('scroll', scrollAnim);
+  window.addEventListener('scroll',scrollAnim);
   scrollAnim();
 });
